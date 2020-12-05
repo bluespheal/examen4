fd013efdbcc87f18d145040f281cc17f8217b52f
José María Beristain <josemaria_1998@outlook.com>
2020-12-04 19:56:07 -0600

    bolas y mesa prefabs

---
+++ Assets/bola.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bola : MonoBehaviour
+{
+    Rigidbody rigi;
+    Collider col;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigi = gameObject.GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.CompareTag("mesa"))
+        {
+            rigi.constraints = RigidbodyConstraints.FreezePositionY;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform.CompareTag("goal")) {
+            col.enabled = false;
+            rigi.constraints = RigidbodyConstraints.None;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
